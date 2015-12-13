0974b6168960027a4f9641e62e98c706eca0e1b9
Martin Wilz <github@wilz.de>
2015-12-13 16:11:09 +0100

    added a first test implementation

--- tests/test.c
+++ tests/test.c
@@ -3,12 +3,15 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-#include <arguments.h>
+#include "arguments.h"
 
 void test_getopt_config()
 {
+  const char* argv[] = {"test", "abcde"};
+  struct ProcessOptions options = parse_command_line(2, argv);
+  assert_non_null(options.command);
+  assert_string_equal(options.command, argv[1]);
 }
-
 
 int main(void)
 {
