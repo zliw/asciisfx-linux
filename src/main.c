1aab1392719ed9fe71e74a6addba6cb59f67b5b9
Martin Wilz <github@wilz.de>
2015-12-13 12:31:20 +0100

    first take on command line parsing

--- src/main.c
+++ src/main.c
@@ -1,3 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+struct ProcessOptions {
+    int  verbose;
+    char *output_filename;
+    char *command;
+};
+
+void print_usage() {
+    printf("usage: asciisfx [--output filename.wav] command\n");
+    exit(-1);
+}
+
+struct ProcessOptions parse_command_line(int argc, char *argv[]) {
+    struct ProcessOptions options = {0, 0, ""};
+
+    struct option long_options[] = {
+        {"verbose", no_argument, &options.verbose, 1},
+        {"output",  required_argument, 0, 'o'},
+        {0, 0, 0, 0}
+    };
+
+    int c;
+
+    while (1) {
+        int option_index = 0;
+
+        c = getopt_long(argc, argv, "o:",
+                        long_options, &option_index);
+
+        switch (c) {
+            case -1:
+                if (optind == argc - 1) {
+                    options.command = argv[optind];
+                    return options;
+                }
+                print_usage();
+
+            case 0:
+                break;
+
+            case 'o':
+                options.output_filename = optarg;
+                break;
+
+            default:
+                print_usage();
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
-	return 0;
+    struct ProcessOptions options = parse_command_line(argc, argv);
+    return 0;
 }
