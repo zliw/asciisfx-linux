4bdc011a8edcf618e5ae713f56d700076f886bc6
Martin Wilz <github@wilz.de>
2015-12-28 13:14:49 +0100

    more playback setup

--- src/playback.c
+++ src/playback.c
@@ -17,6 +17,7 @@ int play(struct Buffer buffer) {
     unsigned int err;
     snd_pcm_t *pcm_handle;
     snd_pcm_hw_params_t *params;
+    snd_pcm_uframes_t frame_size;
 
 	  /* Open the PCM device in playback mode */
 	  if (alsa_device =  (&pcm_handle, PCM_DEVICE,
@@ -34,18 +35,18 @@ int play(struct Buffer buffer) {
 
     if (err = snd_pcm_hw_params_set_access(pcm_handle, params,
                                  					 SND_PCM_ACCESS_RW_INTERLEAVED) < 0) {
-	   	  printf("ERROR: Can't interleaved set mode. %s\n", snd_strerror(err));
+	   	  fprintf(stderr, "ERROR: Can't interleaved set mode. %s\n", snd_strerror(err));
         return 0;
     }
 
 	  if (err = snd_pcm_hw_params_set_format(pcm_handle, params,
 	                               					 SND_PCM_FORMAT_FLOAT) < 0) {
-    		printf("ERROR: Can't set float format. %s\n", snd_strerror(err));
+    		fprintf(stderr, "ERROR: Can't set float format. %s\n", snd_strerror(err));
     		return 0;
   	}
 
     if (err = snd_pcm_hw_params_set_channels(pcm_handle, params, 1) < 0) {
-    		printf("ERROR: Can't set mono format. %s\n", snd_strerror(err));
+    		fprintf(stderr, "ERROR: Can't set mono format. %s\n", snd_strerror(err));
     		return 0;
     }
 
@@ -53,14 +54,21 @@ int play(struct Buffer buffer) {
     int rate = RATE;
 
     if (err = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0) {
-    		printf("ERROR: Can't set rate. %s\n", snd_strerror(err));
+    		fprintf(stderr, "ERROR: Can't set rate. %s\n", snd_strerror(err));
     		return 0;
     }
 
   	if (err = snd_pcm_hw_params(pcm_handle, params) < 0) {
-    		printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(err));
+    		fprintf(stderr, "ERROR: Can't set hardware parameters. %s\n", snd_strerror(err));
         return 0;
   	}
+
+    if (err = snd_pcm_hw_params_get_period_size(params, &frame_size, 0) < 0) {
+        fprintf(stderr, "ERROR: Can't determine frame size for current parameters. %s\n", snd_strerror(err));
+        return 0;
+    }
+
+    fprintf(stdout, "playback with frame size %ld", frame_size);
 
 
     return 1;
