92d5228817ddfcc638a59c2835b57b1f3545d43f
Martin Wilz <github@wilz.de>
2015-12-28 17:35:57 +0100

    basic untested playback loop

--- src/playback.c
+++ src/playback.c
@@ -12,12 +12,16 @@ struct Buffer {
   float *data;
 };
 
-int play(struct Buffer buffer) {
-    unsigned int alsa_device;
-    unsigned int err;
+uint32_t play_buffer_on_pcm_handle(struct Buffer buffer,
+                                   snd_pcm_t *pcm_handle,
+                                   snd_pcm_uframes_t frame_size);
+
+
+uint32_t play(struct Buffer buffer) {
+    uint32_t alsa_device;
+    int err;
     snd_pcm_t *pcm_handle;
     snd_pcm_hw_params_t *params;
-    snd_pcm_uframes_t frame_size;
 
 	  /* Open the PCM device in playback mode */
 	  if (alsa_device =  (&pcm_handle, PCM_DEVICE,
@@ -50,7 +54,6 @@ int play(struct Buffer buffer) {
     		return 0;
     }
 
-
     int rate = RATE;
 
     if (err = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0) {
@@ -63,13 +66,41 @@ int play(struct Buffer buffer) {
         return 0;
   	}
 
+
+    snd_pcm_uframes_t frame_size;
     if (err = snd_pcm_hw_params_get_period_size(params, &frame_size, 0) < 0) {
         fprintf(stderr, "ERROR: Can't determine frame size for current parameters. %s\n", snd_strerror(err));
         return 0;
     }
 
-    fprintf(stdout, "playback with frame size %ld", frame_size);
+    uint32_t result = play_buffer_on_pcm_handle(buffer, pcm_handle, frame_size);
 
+  	snd_pcm_drain(pcm_handle);
+  	snd_pcm_close(pcm_handle);
 
+    return result;
+}
+
+uint32_t play_buffer_on_pcm_handle(struct Buffer buffer,
+                                   snd_pcm_t *pcm_handle,
+                                   snd_pcm_uframes_t frame_size) {
+    uint32_t left = buffer.length;
+    uint32_t position;
+    int err;
+
+    fprintf(stdout, "playback with frame size %ld\n", frame_size);
+
+  	for (position = 0; position < buffer.length; position += frame_size, left -= buffer.length) {
+  	    void *buffer_start = &buffer.data[position];
+  	    uint32_t size = (left < frame_size) ? left : frame_size;
+
+    		while (err = snd_pcm_writen(pcm_handle, &buffer_start, size) == -EPIPE) {
+	  		    snd_pcm_prepare(pcm_handle);
+	  	  }
+	  	  if (err < 0) {
+	    	  	fprintf(stderr, "ERROR. Can't write to PCM device. %s\n", snd_strerror(err));
+	    	  	return 0;
+	    	}
+  	}
     return 1;
 }
